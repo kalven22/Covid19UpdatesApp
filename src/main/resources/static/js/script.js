07a6011b027bd86c3c08a71ea45541fb6f153818
Kalven <kalven22@outlook.com>
2020-03-17 19:20:08 -0400

    36

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -2,3 +2,9 @@ function funVisible(){
     document.getElementById("globalTable").style.display = "none";
 }
 
+$(document).ready(function(){
+    $("#firstcard").click(function(){
+        $("#globalTable").toggle();
+    });
+});
+

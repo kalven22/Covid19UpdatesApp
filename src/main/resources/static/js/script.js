812dc651ddf4aa9bab268ca039312c7ded7957d4
Kalven <kalven22@outlook.com>
2020-03-17 20:06:26 -0400

    38

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,10 +1,28 @@
-function funVisible(){
-    document.getElementById("globalTable").style.display = "none";
-}
-
 $(document).ready(function(){
-    $("#firstcard").click(function(){
-        $("#globalTable").toggle();
+    $("#globalCard").click(function(){
+        $("#globalTable").show();
+        $("#canadaTable").hide();
+        $("#usaTable").hide();
+        $("#indiaTable").hide();
     });
+    $("#canadaCard").click(function(){
+        $("#globalTable").hide();
+        $("#canadaTable").show()
+        $("#usaTable").hide();
+        $("#indiaTable").hide();
+    });
+    $("#usaCard").click(function(){
+        $("#globalTable").hide();
+        $("#canadaTable").hide();
+        $("#usaTable").show();
+        $("#indiaTable").hide();
+    });
+    $("#indiaCard").click(function(){
+        $("#globalTable").hide();
+        $("#canadaTable").hide();
+        $("#usaTable").hide();
+        $("#indiaTable").show();
+    });
+
 });
 
